accd0e153f2dfd1858058bf7fedbd8a5eae05a8b
Juan Aparicio <apariciojuan30@gmail.com>
2020-07-06 12:05:21 +0000

    code review changes

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Resource, type: :model do
-  subject { create :resource }
+  subject { create(:resource) }
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:resource_type) }
