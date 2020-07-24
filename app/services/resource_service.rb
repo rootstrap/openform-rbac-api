e9aec091ed74c3cda3ac30d59231c169c8c2f53a
Juan Aparicio <apariciojuan30@gmail.com>
2020-07-24 15:25:26 +0000

    code_review

--- app/services/resource_service.rb
+++ app/services/resource_service.rb
@@ -5,15 +5,7 @@ class ResourceService
   end
 
   def resource
-    return matching_resources.take! if exists?
-
     Resource.find_or_create_by!(resource_type: @resource_type, resource_id: @resource_id)
-  end
-
-  private
-
-  def exists?
-    matching_resources.empty?
   end
 
   def matching_resources
